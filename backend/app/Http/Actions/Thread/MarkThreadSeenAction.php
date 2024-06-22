ffa311dc56dbcf0505aba1e6349943c75589e363
Regarddeb <unohumphrey260@gmail.com>
2024-06-22 15:17:02 +0800

    action and job for marking a thread as seen when user clicks on it

--- backend/app/Http/Actions/Thread/MarkThreadSeenAction.php
+++ backend/app/Http/Actions/Thread/MarkThreadSeenAction.php
@@ -1,39 +1,23 @@
 <?php
 
-namespace App\Jobs\Threads;
+namespace App\Http\Actions\Thread;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-
+use App\Models\Member;
 use App\Models\Seen_By;
 use App\Models\Chat;
-use App\Models\Member;
 
-class MarkThreadSeen implements ShouldQueue
+class MarkThreadSeenAction
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
     public $thread_id;
 
-    /**
-     * Create a new job instance.
-     */
     public function __construct($thread_id)
     {
         $this->thread_id = $thread_id;
     }
 
-    /**
-     * Execute the job.
-     */
-    public function handle(): void
+    public function execute()
     {
         $thread_id = $this->thread_id;
-
         $member = Member::where('user_id', auth()->id())->where('thread_id', $thread_id)->first();
 
         $latestChat = Chat::latest()->first();
